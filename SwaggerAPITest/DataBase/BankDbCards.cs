7f71d3761009432aeabf199981939c5609d8ec81
Slava <kasterov.v@gmail.com>
2022-10-14 17:09:32 +0300

    Possible to add big preset of cards

--- SwaggerAPITest/DataBase/BankDbCards.cs
+++ SwaggerAPITest/DataBase/BankDbCards.cs
@@ -8,11 +8,20 @@ public class BankDbCards
 {
     public static void Init(BankDbContext db)
     {
+        db.Database.EnsureCreated();
 
         if (!db.Cards.Any())
         {
-            db.Cards.Add(new("4444333322221111", "Troy Mcfarland", "edyDfd5A", CardBrands.Visa, 800));
-            db.Cards.Add(new("5200000000001005", "Levi Downs", "teEAxnqg", CardBrands.MasterCard, 400));
+            Card[] cards = {
+
+                new("4444333322221111", "Troy Mcfarland", "edyDfd5A", CardBrands.Visa, 800),
+                new("5200000000001005", "Levi Downs", "teEAxnqg", CardBrands.MasterCard, 400)
+            };
+
+            foreach (Card card in cards)
+            {
+                db.Cards.Add(card);
+            }
             db.SaveChanges();
         }
     }
